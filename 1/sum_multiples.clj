c3af477fb1c5605f09999a8bd35af209c9390e41
Jacob Swanner <jacob@envylabs.com>
2011-08-30 15:07:52 -0400

    completed first challenge

---
+++ 1/sum_multiples.clj
@@ -0,0 +1,21 @@
+(ns sum-multiples)
+
+(defn is-multiple-of
+  ([number] (fn [divisor] (= 0
+                             (rem number divisor)))))
+
+(defn is-multiple-of-some
+  ([divisors number] (not= nil
+                           (some (is-multiple-of number)
+                                 divisors))))
+
+(defn find-multiples [divisors limit]
+  (loop [numbers (range 1 limit)
+         multiples []]
+    (if (and numbers)
+      (recur (next numbers)
+             (let [number (first numbers)]
+               (if (is-multiple-of-some divisors number)
+                 (conj multiples number)
+                 multiples)))
+      multiples)))
---
+++ 1/sum_multiples_test.clj
@@ -0,0 +1,59 @@
+(ns sum-multiples-test
+  (:use [sum-multiples])
+  (:use [clojure.test]))
+
+(deftest test-is-multiple-of
+  (is (= false
+         ((is-multiple-of 1) 3)))
+  (is (= true
+         ((is-multiple-of 3) 3)))
+  (is (= false
+         ((is-multiple-of 4) 3)))
+  (is (= true
+         ((is-multiple-of 6) 3)))
+  (is (= false
+         ((is-multiple-of 1) 5)))
+  (is (= false
+         ((is-multiple-of 3) 5)))
+  (is (= true
+         ((is-multiple-of 5) 5)))
+  (is (= false
+         ((is-multiple-of 6) 5)))
+  (is (= true
+         ((is-multiple-of 10) 5))))
+
+(deftest test-is-multiple-of-some
+  (is (= false
+         (is-multiple-of-some [3 5] 1)))
+  (is (= false
+         (is-multiple-of-some [3 5] 2)))
+  (is (= true
+         (is-multiple-of-some [3 5] 3)))
+  (is (= false
+         (is-multiple-of-some [3 5] 4)))
+  (is (= true
+         (is-multiple-of-some [3 5] 5)))
+  (is (= true
+         (is-multiple-of-some [3 5] 6)))
+  (is (=
+         (is-multiple-of-some [3 5] 7)))
+  (is (= false
+         (is-multiple-of-some [3 5] 8)))
+  (is (= true
+         (is-multiple-of-some [3 5] 9)))
+  (is (= true
+         (is-multiple-of-some [3 5] 10))))
+
+(deftest test-find-multiples-10
+  (is (= [3 5 6 9]
+         (find-multiples [3 5] 10))))
+
+(deftest test-find-multiples-15
+  (is (= [3 5 6 9 10 12]
+         (find-multiples [3 5] 15))))
+
+(deftest test-find-multiples-20
+   (is (= [3 5 6 9 10 12 15 18]
+          (find-multiples [3 5] 20))))
+
+(run-tests)
